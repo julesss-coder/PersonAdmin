6c039962cef339a54fbcc61ff0e556a6ce49bc1f
Julia <67640854+julesss-coder@users.noreply.github.com>
2023-01-16 15:16:14 +0100

    Added access modifier

--- PersonManager.java
+++ PersonManager.java
@@ -4,25 +4,42 @@ import java.time.LocalDate;
 import java.util.LinkedList;
 
 public class PersonManager {
-    LinkedList<Person> persons = new LinkedList<Person>();
+    private LinkedList<Person> persons = new LinkedList<Person>();
 
     // Object methods
-    void createPerson(String firstName, String lastName) {
+    public void createPerson(String firstName, String lastName) {
         Person person = new Person(firstName, lastName);
         persons.add(person);
     }
 
-    void createPerson(String firstName, String lastName, LocalDate dateOfBirth, Address address, Sex sex) {
+    public void createPerson(String firstName, String lastName, LocalDate dateOfBirth, Address address, Sex sex) {
         Person person = new Person(firstName, lastName, dateOfBirth, address, sex);
         persons.add(person);
     }
 
-    void createPerson(String firstName, String lastName, LocalDate dateOfBirth, Sex sex) {
+    public void createPerson(String firstName, String lastName, LocalDate dateOfBirth, Sex sex) {
         Person person = new Person(firstName, lastName, dateOfBirth, sex);
         persons.add(person);
     }
 
-    void deletePerson(String lastName) {
+    public void getPerson(String lastName) {
+        for (Person person: persons) {
+            if (person.lastName.equals(lastName)) {
+                System.out.println(person.toString());
+                return;
+            }
+        }
+    }
+
+    public void listPersons() {
+        System.out.println("Current list of persons:");
+        for (Person person: persons) {
+            System.out.println(person.toString());
+        }
+    }
+
+    // A getter/setter gets or sets the fields of this class, so this is not a getter/setter, as it changes only a single entry in `persons`:
+    public void deletePerson(String lastName) {
         for (Person person: persons) {
             if (person.lastName.equals(lastName)) {
                 persons.remove(person);
